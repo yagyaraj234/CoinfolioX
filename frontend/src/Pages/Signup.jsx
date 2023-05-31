d06db3e852248845ed423e2588a9a25c7973b887
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-31 16:54:30 +0530

    login & signup

---
+++ frontend/src/Pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Signup = () => {
+  return (
+    <Layout>
+      Signup
+    </Layout>
+  )
+}
+
+export default Signup
