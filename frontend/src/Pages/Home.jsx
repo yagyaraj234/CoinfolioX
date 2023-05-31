5f1c4596c23cad38656ea61323620fcb35247afb
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-31 16:54:42 +0530

    home section

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import NewsCard from '../components/NewsCard'
+
+
+
+const Home = () => {
+  return (
+    <Layout>
+    <NewsCard/>
+</Layout>
+  )
+}
+
+export default Home
