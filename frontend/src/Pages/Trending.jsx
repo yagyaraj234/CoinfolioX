6b3bb742ddb07d25ff5cdb33833b98cb9c991bdd
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-31 16:54:54 +0530

    trending

---
+++ frontend/src/Pages/Trending.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Trending = () => {
+  return (
+    <Layout>
+        Trending
+    </Layout>
+  )
+}
+
+export default Trending
