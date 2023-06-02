be64ea5730d29ef3881c46988ad5a2426d9545a3
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-02 21:34:36 +0530

    sample data added

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { newData } from '../data'
 
 const NewsCard = () => {
     return (
         <div
-            className='grid md:grid-cols-3 grid-cols-1 gap-20  py-10 '>
-            <div className=" border p-4 flex flex-col  rounded-md shadow-md bg-gray-400">
-                <p className='text-left text-sm pb-4'>Hello World <span></span></p>
-                <p className='text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, quia? Modi, voluptatem dolore totam itaque commodi unde reiciendis, harum eveniet dicta iure sapiente? Aperiam suscipit necessitatibus maxime ...... </p>
-            </div>
-            
+            className='grid md:grid-cols-3 grid-cols-1 gap-20'>
+            {newData.map((data) => (
+                <div className=" border p-4 flex flex-col  rounded-md shadow-md " key={data.key}>
 
+                    <p className='text-left text-sm pb-2 mb-2 border-b'>{data.username} <span></span></p>
+                    <p className='text-justify'>{data.news}</p>
+                </div>
+            ))}
         </div>
     )
 }
