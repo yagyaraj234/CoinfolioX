c661a3cf329722ad55f2eee9927a7f25b7949a9f
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-05 17:20:31 +0530

    signup page

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { NavLink } from 'react-router-dom'
+
 
 const Signup = () => {
   return (
     <Layout>
-      Signup
+      <div className='mx-auto w-full md:w-1/4  md:pb-6 md:pt-5'>
+        <input type="text" placeholder='Username' className=' rounded p-2 w-full border mb-4' />
+        <input type="text" placeholder='Email' className='rounded p-2 w-full border mb-4' />
+
+        <input type="text" placeholder='Password' className='rounded p-2 w-full border mb-4' />
+        <input type="text" placeholder='Confirm Password' className='rounded p-2 w-full border' />
+        <p className=' text-sm mb-4'>Already have Account Login  <NavLink className='cursor-pointer text-blue-600' to='/login'>here.</NavLink></p>
+        <button className='rounded p-2 w-full border mb-4'>Signup</button>
+
+
+      </div>
     </Layout>
   )
 }
