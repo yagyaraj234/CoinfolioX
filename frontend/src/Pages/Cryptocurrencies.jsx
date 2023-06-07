4a6007b73d93c0fb4549fadf00e857b3fe1147b1
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-07 21:00:53 +0530

    inr and usd conversion added

--- frontend/src/Pages/Cryptocurrencies.jsx
+++ frontend/src/Pages/Cryptocurrencies.jsx
@@ -1,60 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../components/Layout/Layout'
-import { NavLink } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Layout from "../components/Layout/Layout";
+import axios from "axios";
 
 const Cryptocurrencies = () => {
+  const [cryptoData, setCryptoData] = useState([]);
 
-    const [cryptoData, setCryptoData] = useState([]);
+  const [currency, setCurrency] = useState("Usd");
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en`
+      )
+      .then((res) => {
+        setCryptoData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <Layout>
+      <div className="flex md:flex-row flex-col md:justify-between md:items-center">
+        <h1 className="md:text-2xl text-xl font-semibold md:pb-10 pb-2">
+          Cryptocurrency Prices by Market Cap
+        </h1>
+        <div className="flex border rounded-lg font-normal transition-colors md:mb-0 w-[90px]  mb-2 ">
+          <button
+            className={`px-2 rounded-l-lg ${
+              currency === "Inr" && " text-white bg-blue-700 "
+            }`}
+            onClick={() => setCurrency("Inr")}
+          >
+            INR
+          </button>
+          <button
+            className={` px-2 rounded-r-lg  ${
+              currency === "Usd" && "bg-blue-700 text-white"
+            }`}
+            onClick={() => setCurrency("Usd")}
+          >
+            USD
+          </button>
+        </div>
+      </div>
 
-    useEffect(() => {
-        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=Usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en')
-            .then(res => {
-                setCryptoData(res.data);
-            }).catch(err => console.log(err));
-    }, [])
-    return (
-        <Layout>
-            <h1 className='md:text-2xl text-xl font-semibold pb-20'>Cryptocurrency Prices by Market Cap</h1>
+      <ul>
+        <ul className="flex justify-between border text-center items-center ">
+          <li className="border w-1/2 text-gray-800 py-2 px-1 md:px-3">Rank</li>
+          <li className="border md:w-10/12 w-2/3  flex flex-row justify-center items-center py-2 px-1 ">
+            <p className={`hid pl-1 `}></p>
+            <p className="uppercase text-gray-400 text-center pl-2 px-1"></p>
+            Name
+          </li>
+          <li className="border md:w-1/2 w-4/5 py-2 text-center px-1">Price</li>
+          <li className="hid border md:w-1/2 py-2 px-1">24h high</li>
+          <li className="hid border md:w-1/2 py-2 px-1">24h low</li>
+          <li className="border hid w-full py-2 text-center px-1">
+            Total Volume
+          </li>
+          <li className="border w-full py-2 text-center px-1">Market Cap</li>
+        </ul>
 
-            <ul>
-                <ul className='grid grid-cols-4 md:grid-cols-7  font-semibold text-center border-black border-2 text-sm md:text-lg bg-gray-200 '>
-                    <li className='border-black border '>Rank</li>
-                    <li className='border border-black'>Name</li>
-                    {/* <li className='border border-black'>Symbol</li> */}
-                    <li className='border border-black ' >Price</li>
-                    <li className='border border-black hid ' >24 hour low</li>
-                    <li className='border border-black hid ' >24 hour high</li>
-                    <li className='border border-black ' >Total Volume</li>
-                    <li className='border border-black hid' >Market cap</li>
-                </ul>
-
-                {cryptoData?.map((coin) => (
-                    <ul className='grid  grid-cols-4 md:grid-cols-7  text-center text-xs md:text- font-medium items-center'>
-                        <NavLink className='border py-2'>{coin.market_cap_rank}</NavLink>
-                        <NavLink className='border flex flex-row gap-3 justify-start pl-4 items-center py-2   '>
-                            <img className='h-4 w-4 ' src={coin.image} alt="logo" />
-                            <p className='hid'>{(coin.name).slice(0,12)}</p>
-                            
-                            <p className='uppercase text-gray-400 text-right'>{coin.symbol}</p>
-                            </NavLink>
-                        {/* <NavLink className='border py-2'>{coin.symbol}</NavLink> */}
-                        <NavLink className='border py-2'>$ {coin.current_price}</NavLink>
-                        <li className='border hid py-2 ' >$ {coin.low_24h}</li>
-                        <li className='border hid py-2 ' >$ {coin.high_24h}</li>
-                        <NavLink className='border py-2'>$ {coin.total_volume}</NavLink>
-                        <li className='border hid py-2' >$ {coin.market_cap}</li>
-
-
-                    </ul>
-                ))}
-            </ul>
-        </Layout>
-    )
-}
+        {cryptoData?.map((coin) => (
+          <ul
+            key={coin.market_cap_rank}
+            className="flex justify-between border text-center items-center "
+          >
+            <li className="border w-1/2 text-gray-800 py-2 px-1 md:px-3">
+              {coin.market_cap_rank}
+            </li>
+            <li className="border md:w-10/12 w-2/3  flex flex-row justify-start items-center py-2 px-1 ">
+              <img className="h-4 md:pl-3 pl-1 " src={coin.image} alt="logo" />
+              <p className={`hid pl-1 `}>{coin.name.slice(0, 12)}</p>
+              <p className="uppercase text-gray-400 text-center pl-2 px-1">
+                {coin.symbol}
+              </p>
+            </li>
+            <li className="border md:w-1/2 w-4/5 py-2 text-center px-1">
+            <span>{currency==='Inr'?'₹':'$'}</span>{coin.current_price}
+            </li>
+            <li className="hid border md:w-1/2 py-2 px-1"><span>{currency==='Inr'?'₹':'$'}</span>{coin.high_24h}</li>
+            <li className="hid border md:w-1/2 py-2 px-1"><span>{currency==='Inr'?'₹':'$'}</span>{coin.low_24h}</li>
+            <li className="border hid w-full py-2 text-center px-1">
+            <span>{currency==='Inr'?'₹':'$'}</span>
+              {coin.total_volume}
+            </li>
+            <li className="border w-full py-2 text-center px-1">
+                <span>{currency==='Inr'?'₹':'$'}</span>
+              {coin.market_cap}
+            </li>
+          </ul>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
 export default Cryptocurrencies;
-
-
-
