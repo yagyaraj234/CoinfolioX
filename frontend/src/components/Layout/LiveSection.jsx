1ca1246d232e09e3f5d2b5093e34ad032362eb4e
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-09 00:00:58 +0530

    btc eth price added

--- frontend/src/components/Layout/LiveSection.jsx
+++ frontend/src/components/Layout/LiveSection.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const LiveSection = () => {
-  return (
-    <div className='md:px-10 px-2 py-1 border-b-2 '>LiveSection</div>
-  )
-}
+  let [btcPrice, setBtcPrice] = useState([]);
+  let [ethPrice, setEthPrice] = useState([]);
 
-export default LiveSection
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+      )
+      .then((res) => {
+        setBtcPrice(res.data.bitcoin.usd);
+        setEthPrice(res.data.ethereum.usd);
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err));
+  });
+  return (
+    <div className="md:px-10 px-2 py-1 border-b-2 flex justify-between  ">
+      <p>LiveSection</p>
+      <div className="flex gap-1 text-sm">
+        <p >
+          Btc: <span className=" text-blue-600  ">{btcPrice}</span>
+        </p>
+        <p>
+          Eth: <span className="text-blue-600">{ethPrice}</span>
+        </p>
+        {/* <p>Eth:{}</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default LiveSection;
