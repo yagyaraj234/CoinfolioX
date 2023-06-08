79155930fe542836e3fdeacb8547a237a21cfa62
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-08 15:42:08 +0530

    crousel added

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 import NewsCard from '../components/NewsCard'
 
+
 const Home = () => {
   return (
     <Layout>
       <NewsCard />
-
     </Layout>
   )
 }
--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
-import { newData } from '../data'
+import React from "react";
+import { newData } from "../data";
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 3 },
+};
 
 const NewsCard = () => {
-    return (
-        <div
-            className='grid md:grid-cols-3 grid-cols-1 gap-20'>
-            {newData.map((data) => (
-                <div className=" border p-4 flex flex-col  rounded-md shadow-md " key={data.key}>
+  return (
+    <>
+      <AliceCarousel responsive={responsive}
+      mouseTracking={true}
+      autoPlayInterval={4000}
+      autoPlay={true}
+      infinite={true}
+      disableDotsControls={true}
+      >
+        {newData.map((data) => (
+          <div
+            className=" border p-4 flex flex-col  rounded-md shadow-md min-h-[180px] max-h-[180px] mx-5 bg-white "
+            key={data.key}
+          >
+            <p className="text-left text-sm pb-2 mb-2 border-b">
+              {data.username} <span></span>
+            </p>
+            <p className="text-justify pb-2">
+              {data.news.slice(0, 170)}
+              <span className="block text-blue-700 font-semibold">
+                {data.news.length >= "500" && " read more"}
+              </span>
+            </p>
+          </div>
+        ))}
+      </AliceCarousel>
+    </>
+  );
+};
 
-                    <p className='text-left text-sm pb-2 mb-2 border-b'>{data.username} <span></span></p>
-                    <p className='text-justify'>{data.news}</p>
-                </div>
-            ))}
-        </div>
-    )
-}
-
-export default NewsCard
+export default NewsCard;
