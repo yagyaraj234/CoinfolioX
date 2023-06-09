9bc14f9da09a14b26283abee3f13385b7282d983
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-09 12:25:30 +0530

    butoon added and coin list

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 import NewsCard from '../components/NewsCard'
+import CoinList from '../components/CoinList'
+import { NavLink } from 'react-router-dom'
 
 
 const Home = () => {
   return (
     <Layout>
       <NewsCard />
+      <CoinList amount={50}/>
+      <button className=' flex justify-center mx-auto  px-2 py-1 rounded-md bg-blue-600 text-white hover:bg-blue-700 w-2/4 my-5 md:py-2 md:w-1/4'><NavLink to='/cryptocurrencies'>More Coins</NavLink></button>
     </Layout>
   )
 }
