17ce04288bf9969573de959b69591f668f01dc1b
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-07-28 20:56:25 +0530

    footer updated

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,66 +1,106 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const dropDown = [
   {
-    id: 1, name: 'Resources', items: [{
-      item: 'Perceptuals'
-    }, {
-      id: 2, item: 'Crypto News'
-    }]
+    id: 1,
+    name: "Resources",
+    items: [
+      {
+        item: "Perceptuals",
+      },
+      {
+        id: 2,
+        item: "Crypto News",
+      },
+    ],
   },
   {
-    name: 'Donations', items: [{ item: 'Bitcoin' }, { item: 'Ethereum' }]
-  }
-]
+    name: "Donations",
+    items: [{ item: "Bitcoin" }, { item: "Ethereum" }],
+  },
+];
 
 const Footer = () => {
+  const [showDropDown, setShowDropDown] = useState({});
 
-  const [showDropDown, setShowDropDown] = useState(false)
+  // Function to toggle the visibility of a specific dropdown category
+  const toggleDropDown = (id) => {
+    setShowDropDown((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
   return (
-    <div className='absolute w-full'>
-      <div className='flex flex-col  md:px-10 px-2 py-2 md:w-full  relative bottom-0 border-t-2'>
+    <div className="absolute w-full">
+      <div className="flex flex-col  md:px-10 px-2 py-2 md:w-full  relative bottom-0 border-t-2">
         <div className="">
           {/* <p </p> */}
-          <Link to='/' className='text-2xl font-bold text-blue-800 text-left '>CoinFolioX</Link>
+          <Link to="/" className="text-2xl font-bold text-blue-800 text-left ">
+            CoinFolioX
+          </Link>
         </div>
         <div className="border-b-2 py-4 text-justify">
-          <p>Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptates aliquid obcaecati accusantium eligendi facilis nostrum, magnam numquam quo corporis reiciendis.
+          <p>
+            Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Quidem voluptates aliquid obcaecati accusantium eligendi facilis
+            nostrum, magnam numquam quo corporis reiciendis.
           </p>
         </div>
         <div className="flex flex-col justify-center items-start">
-
-          <ul className=' '>
-            {dropDown.map((dropD) => (
-              <li key={dropD.id} className={`cursor-pointer text-md   w-full
-             border-b-2 ${showDropDown ? 'py-0' : 'py-3'} font-semibold `} onClick={() => setShowDropDown(!showDropDown)}>
-                {dropD.name}
+          <ul className=" ">
+            {dropDown.map((drop) => (
+              <li
+                key={drop.id}
+                className={`cursor-pointer text-md   w-full
+             border-b-2 ${showDropDown[drop.id] ? "py-0" : "py-3"} font-semibold `}
+                onClick={() => toggleDropDown(drop.id)} // Toggle visibility for the specific category
+              >
+                {drop.name}
                 {/* <p> fsjk</p> */}
                 <div>
-                  <ul className={`flex flex-col text-sm text-gray-600  ${!showDropDown && 'hidden'}`}>
-                    {dropD.items.map((subD) => (
-                      <li className=' border-b py-3' > {subD.item}</li>
-
+                  <ul
+                    className={`flex flex-col text-sm text-gray-600  ${
+                      !showDropDown[drop.id] && "hidden"
+                    }`}
+                  >
+                    {drop.items.map((subD, index) => (
+                      <li className="border-b py-3" key={index}>
+                        {subD.item}
+                      </li>
                     ))}
                   </ul>
                 </div>
-
               </li>
             ))}
-
           </ul>
         </div>
 
+        {/* Newsletter Subscription Section (Unchanged) */}
         <div>
-          <p className='font-semibold'>Interested to stay up-to-date with cryptocurrencies?</p>
-          < p className='text-sm text-gray-600'>Get the latest crypto news, updates, and reports by subscribing to our free newsletter.</p>
-          <input type="text" className='border border-gray-500 rounded-md w-full py-1 my-3 px-3' />
-          <button type='submit' className='border w-full py-1 rounded-md bg-green-600 text-white'>Subscribe</button>
+          <p className="font-semibold">
+            Interested to stay up-to-date with cryptocurrencies?
+          </p>
+          <p className="text-sm text-gray-600">
+            Get the latest crypto news, updates, and reports by subscribing to
+            our free newsletter.
+          </p>
+          <input
+            type="text"
+            className="border border-gray-500 rounded-md w-full py-1 my-3 px-3"
+          />
+          <button
+            type="submit"
+            className="border w-full py-1 rounded-md bg-green-600 text-white"
+          >
+            Subscribe
+          </button>
         </div>
-        <p className='mx-auto my-2 text-gray-600'> &copy; 2023 CoinFolioX. All Right Reserved. </p>
+        {/* Copyright Notice (Unchanged) */}
+        <p className="mx-auto my-2 text-gray-600">
+          &copy; 2023 CoinFolioX. All Right Reserved.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
