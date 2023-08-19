48221f18af6b81728393bbea4f1fe03412809188
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-20 01:50:53 +0530

    routes added for login and signup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import Express from "express";
 import dotenv from "dotenv";
-import userRegistration from "./controller/user.js";
-import userLogin from "./controller/getUser.js";
+import userRegistration from "./controller/userSignup.js";
+import userLogin from "./controller/userLogin.js";
 import connectDB from "./dbConnect.js";
 import cors from "cors";
 
@@ -11,8 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middlewares
 app.use(Express.json());
-// app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 // Database connection
 connectDB();
