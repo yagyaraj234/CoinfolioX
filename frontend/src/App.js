d40773885083c763506ad97921f1fc0d5a722257
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-20 04:19:56 +0530

    protected routed for login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,14 @@ import {
   Exchanges,
 } from "./Pages/index";
 import CoinDetail from "./Pages/CoinDetail";
+import { useEffect, useState } from "react";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 function App() {
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+  }, []);
   return (
     <div className=" text-justify bg-[#F9FAFD]">
       <Routes>
@@ -23,11 +30,13 @@ function App() {
         <Route path="/coins/:id" element={<CoinDetail />} />
         <Route path="/trending" element={<Trending />} />
         <Route path="/articles" element={<Articles />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/exchanges" element={<Exchanges />} />
 
         {/* Login  Signup routes  */}
-        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoutes token={token} />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
         <Route path="/signup" element={<Signup />} />
 
         {/* Universal route/ Invalid Route  */}
