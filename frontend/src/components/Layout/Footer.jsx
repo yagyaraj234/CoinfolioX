783a54f85e2830213fcb500624e59379a42a4397
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-20 05:10:16 +0530

    footer email subscription validation added

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
+import * as yup from "yup";
+import { Formik, ErrorMessage, Field, Form, useFormikContext } from "formik";
 
 const dropDown = [
   {
@@ -27,23 +29,65 @@ const dropDown = [
   },
 ];
 
+const initialValue = {
+  email: "",
+};
+
+const validate = yup.object({
+  email: yup
+    .string()
+    .email("Please enter a valid email ")
+    .required("Email is required"),
+});
+
+const SubscriptionForm = () => {
+  const { touched } = useFormikContext();
+
+  return (
+    <div>
+      <p className="font-semibold">
+        Interested to stay up-to-date with cryptocurrencies?
+      </p>
+      <p className="text-sm text-gray-600">
+        Get the latest crypto news, updates, and reports by subscribing to our
+        free newsletter.
+      </p>
+      <Form>
+        <div>
+          <Field
+            type="email"
+            id="email"
+            name="email"
+            className={`border border-gray-500 rounded-md w-full py-1 mt-3 px-3 outline-none ${
+              touched.email && "border-2 border-red-600"
+            }  `}
+          />
+          <ErrorMessage name="email">
+            {(msg) =>
+              touched.email ? (
+                <div className="border-red-500 text-red-600 text-sm">{msg}</div>
+              ) : null
+            }
+          </ErrorMessage>
+        </div>
+        <button
+          type="submit"
+          className="border mt-2 w-full py-1 rounded-md bg-green-600 text-white"
+        >
+          Subscribe
+        </button>
+      </Form>
+    </div>
+  );
+};
+
 const Footer = () => {
   const [showDropDown, setShowDropDown] = useState({});
-  const [email, setEmail] = useState("");
-  const [button, setButton] = useState(false);
-
-  useEffect(() => {
-    if (email.length > 8) {
-      setButton(true);
-    }
-  }, [email]);
 
   const subscibe = () => {
     toast.success("Thank you For subscribing");
-    setEmail("");
   };
 
-  // Function to toggle the visibility of a specific dropdown category
   const toggleDropDown = (id) => {
     setShowDropDown((prev) => ({ ...prev, [id]: !prev[id] }));
   };
@@ -51,13 +95,10 @@ const Footer = () => {
   return (
     <div className="absolute w-full">
       <Toaster />
-      <div className="flex flex-col  md:px-10 px-2 py-2 md:w-full  relative bottom-0 border-t-2">
-        <div className="">
-          {/* <p </p> */}
-          <Link to="/" className="text-2xl font-bold text-blue-800 text-left ">
-            CoinFolioX
-          </Link>
-        </div>
+      <div className="flex flex-col md:px-10 px-2 py-2 md:w-full relative bottom-0 border-t-2">
+        <Link to="/" className="text-2xl font-bold text-blue-800 text-left">
+          CoinFolioX
+        </Link>
         <div className="border-b-2 py-4 text-justify">
           <p>
             Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -66,21 +107,19 @@ const Footer = () => {
           </p>
         </div>
         <div className="flex flex-col justify-center items-start">
-          <ul className=" ">
+          <ul>
             {dropDown.map((drop) => (
               <li
                 key={drop.id}
-                className={`cursor-pointer text-md   w-full
-             border-b-2 ${
-               showDropDown[drop.id] ? "py-2" : "py-2"
-             } font-semibold `}
-                onClick={() => toggleDropDown(drop.id)} // Toggle visibility for the specific category
+                className={`cursor-pointer text-md w-full border-b-2 ${
+                  showDropDown[drop.id] ? "py-2" : "py-2"
+                } font-semibold `}
+                onClick={() => toggleDropDown(drop.id)}
               >
                 {drop.name}
-                {/* <p> fsjk</p> */}
                 <div>
                   <ul
-                    className={`flex flex-col text-sm text-gray-600  ${
+                    className={`flex flex-col text-sm text-gray-600 ${
                       !showDropDown[drop.id] && "hidden"
                     }`}
                   >
@@ -95,31 +134,13 @@ const Footer = () => {
             ))}
           </ul>
         </div>
-
-        {/* Newsletter Subscription Section (Unchanged) */}
-        <div>
-          <p className="font-semibold">
-            Interested to stay up-to-date with cryptocurrencies?
-          </p>
-          <p className="text-sm text-gray-600">
-            Get the latest crypto news, updates, and reports by subscribing to
-            our free newsletter.
-          </p>
-
-          <input
-            type="email"
-            className="border border-gray-500 rounded-md w-full py-1 my-3 px-3"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button
-            onClick={subscibe}
-            type="submit"
-            className="border w-full py-1 rounded-md bg-green-600 text-white"
-          >
-            Subscribe
-          </button>
-        </div>
-        {/* Copyright Notice (Unchanged) */}
+        <Formik
+          initialValues={initialValue}
+          validationSchema={validate}
+          onSubmit={subscibe}
+        >
+          <SubscriptionForm />
+        </Formik>
         <p className="mx-auto my-2 text-gray-600">
           &copy; 2023 CoinFolioX. All Right Reserved.
         </p>
