94e6d13952ad5a64f3af13997a18da30ddd8dff4
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:07:56 +0530

    live sections button added

--- frontend/src/components/Layout/LiveSection.jsx
+++ frontend/src/components/Layout/LiveSection.jsx
@@ -1,32 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useCurrency } from "../Context/CurrencyContext";
+import Dropdown from "../Dropdown";
 
 const LiveSection = () => {
-  let [btcPrice, setBtcPrice] = useState([]);
-  let [ethPrice, setEthPrice] = useState([]);
+  // Replace with actual context values
+  const { currency, symbol } = useCurrency();
+
+  // Initialize state variables with initial values
+  const [btcPrice, setBtcPrice] = useState(0);
+  const [ethPrice, setEthPrice] = useState(0);
 
   useEffect(() => {
+    // Make API call when currency changes
     axios
       .get(
-        `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=Usd`
+        `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=${currency}`
       )
       .then((res) => {
-        setBtcPrice(res.data.bitcoin.usd);
-        setEthPrice(res.data.ethereum.usd);
+        // Update state with new prices
+        setBtcPrice(res.data.bitcoin[currency.toLowerCase()]);
+        setEthPrice(res.data.ethereum[currency.toLowerCase()]);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [currency]);
+
   return (
-    <div className="md:px-10 px-2 py-1 border-b-2 flex justify-between hid  ">
+    <div className="md:px-10 px-2 py-1 border-b-2  justify-between items-center hidden  sm:flex">
       <p>LiveSection</p>
-      <div className="flex gap-1 text-sm">
-        <p>
-          Btc: <span className=" text-blue-600  ">{btcPrice}</span>
-        </p>
-        <p>
-          Eth: <span className="text-blue-600">{ethPrice}</span>
-        </p>
-        {/* <p>Eth:{}</p> */}
+      <div className="flex gap-10">
+        <div className="flex gap-1 mt-1 text-sm">
+          <p>
+            Btc:{" "}
+            <span className="text-blue-600">
+              {symbol}
+              {btcPrice}
+            </span>
+          </p>
+          <p>
+            Eth:{" "}
+            <span className="text-blue-600">
+              {symbol}
+              {ethPrice}
+            </span>
+          </p>
+        </div>
+        <Dropdown />
       </div>
     </div>
   );
