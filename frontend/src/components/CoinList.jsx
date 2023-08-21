1e4e1a6530986fd1659acd3af93a7c2ab69e4a9f
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:06:59 +0530

    coinlist details

--- frontend/src/components/CoinList.jsx
+++ frontend/src/components/CoinList.jsx
@@ -1,139 +1,109 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useCurrency } from "../components/Context/CurrencyContext";
+import { CoinLists } from "../config/api";
 
-const CoinList = ({ amount }) => {
+const CoinList = () => {
   const [cryptoData, setCryptoData] = useState([]);
-  const [currency, setCurrency] = useState("Usd");
   const [filterCoin, setfilterCoin] = useState("");
+  const { currency, symbol } = useCurrency();
+  // https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${amount}&page=1&sparkline=false&locale=en
 
   useEffect(() => {
     axios
-      .get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${amount}&page=1&sparkline=false&locale=en`
-      )
+      .get(CoinLists(currency))
       .then((res) => {
         setCryptoData(res.data);
-        
       })
       .catch((err) => console.log(err));
   }, [currency]);
-  const currChange = () => {
-    setCurrency("Usd");
-  };
+
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="Search here..."
-          className="border-2 rounded-md py-1 px-2"
-          onChange={(e) => setfilterCoin(e.target.value)}
-        />
-      </div>
       <div className="flex md:flex-row flex-col md:justify-between md:items-center">
         <h1 className="md:text-2xl text-md font-semibold py-4">
           Cryptocurrency Prices by Market Cap
         </h1>
-        <div className="flex border-2  rounded-xl font-normal transition-colors md:mb-0 w-[92px]  mb-2 ">
-          <button
-            className={`px-2 rounded-l-lg ${
-              currency === "Inr" && " text-white bg-blue-700 "
-            }`}
-            onClick={() => setCurrency("Inr")}
-          >
-            INR
-          </button>
-          <button
-            className={` px-2 rounded-r-lg  ${
-              currency === "Usd" && "bg-blue-700 text-white"
-            }`}
-            onClick={currChange}
-          >
-            USD
-          </button>
+        <div>
+          <input
+            type="text"
+            placeholder="Search here..."
+            className="border-2 rounded-md py-1 px-2"
+            onChange={(e) => setfilterCoin(e.target.value)}
+          />
         </div>
       </div>
 
       <table className="border-2  text-center my-2 md:text-md text-sm w-full  ">
-        <tr className=" border-b-2">
-          <th className="md:py-2 py-1 md:w-1/12 w-1/8 border-r-2 ">Rank</th>
-          <th className="md:py-2 py-1 md:w-3/12 w-1/5 border-r-2">Name</th>
-          <th className="md:py-2 py-1 border-r-2 md:w-2/12 w-1/5">
-            Price
-            <span className="text-pink-800">
-              {currency === "Usd" ? " ($)" : " (₹)"}
-            </span>
-          </th>
-          <th className="hid md:py-2 py-1 border-r-2 md:w-1/12 ">
-            24h low
-            <span className="text-pink-800">
-              {currency === "Usd" ? " ($)" : " (₹)"}
-            </span>
-          </th>
-          <th className="hid md:py-2 py-1 border-r-2 md:w-1/12">
-            24h high
-            <span className="text-pink-800">
-              {currency === "Usd" ? " ($)" : " (₹)"}
-            </span>
-          </th>
-          <th className="hid md:py-2 py-1 border-r-2 md:w-2/12">
-            24h Volume
-            <span className="text-pink-800">
-              {currency === "Usd" ? " ($)" : " (₹)"}
-            </span>
-          </th>
-          <th className="md:py-2 md:w-2/12   py-1 md:px-2 px-0">
-            Market Cap
-            <span className="text-pink-800">
-              {currency === "Usd" ? " ($)" : " (₹)"}
-            </span>
-          </th>
-        </tr>
+        <tbody>
+          <tr className=" border-b-2">
+            <th className="md:py-2 py-1 md:w-1/12 w-1/8 border-r-2 ">Rank</th>
+            <th className="md:py-2 py-1 md:w-3/12 w-1/5 border-r-2">Name</th>
+            <th className="md:py-2 py-1 border-r-2 md:w-2/12 w-1/5">
+              <span className="text-pink-800">{symbol}</span> Price
+            </th>
+            <th className="hid md:py-2 py-1 border-r-2 md:w-1/12 ">
+              <span className="text-pink-800">{symbol}</span> 24h low
+            </th>
+            <th className="hid md:py-2 py-1 border-r-2 md:w-1/12">
+              <span className="text-pink-800">{symbol}</span> 24h high
+            </th>
+            <th className="hid md:py-2 py-1 border-r-2 md:w-2/12">
+              <span className="text-pink-800">{symbol}</span> 24h Volume
+            </th>
+            <th className="md:py-2 md:w-2/12   py-1 md:px-2 px-0">
+              <span className="text-pink-800">{symbol}</span> Market Cap
+            </th>
+          </tr>
 
-        {cryptoData
-          .filter((coin) => {
-            return filterCoin.toLowerCase() === ""
-              ? coin
-              : coin.name.toLowerCase().startsWith(filterCoin);
-          })
-          ?.map((coin) => (
-            <tr className="border-b-2" key={coin.id}>
-              <td className="border-r-2 text-center px-1 py-2 ">
-                <Link to={`/coins/${coin.id}`}> {coin.market_cap_rank}</Link>
-              </td>
-              <td className=" border-r-2 py-2">
-                <Link className="flex items-center flex-row" to={`/coins/${coin.id}`}>
-                  <img
-                    src={coin.image}
-                    alt="logo"
-                    className="w-4 h-4 md:mx-2 ml-1"
-                  />
-                  <p className="hid">{coin.name}</p>
+          {cryptoData
+            .filter((coin) => {
+              return filterCoin.toLowerCase() === ""
+                ? coin
+                : coin.name.toLowerCase().startsWith(filterCoin.toLowerCase());
+            })
+            ?.map((coin) => (
+              <tr className="border-b-2" key={coin.id}>
+                <td className="border-r-2 text-center px-1 py-2 ">
+                  <Link to={`/coins/${coin.id}`}> {coin.market_cap_rank}</Link>
+                </td>
+                <td className=" border-r-2 py-2">
+                  <Link
+                    className="flex items-center flex-row"
+                    to={`/coins/${coin.id}`}
+                  >
+                    <img
+                      src={coin.image}
+                      alt="logo"
+                      className="w-4 h-4 md:mx-2 ml-1"
+                    />
+                    <p className="hid">{coin.name}</p>
 
-                  <p className="uppercase text-gray-500 text-md overflow-hidden md:mx-2 ">
-                    {coin.symbol}
-                  </p>
-                </Link>
-              </td>
-              <td className="border-r-2 text-center px-1 py-2 ">
-                <Link to={`/coins/${coin.id}`}> {coin.current_price}</Link>
-              </td>
-              <td className="border-r-2 text-center px-1 py-2 hid ">
-                <Link to={`/coins/${coin.id}`}> {coin.low_24h}</Link>
-              </td>
-              <td className="border-r-2 text-center px-1 py-2 hid ">
-                <Link to={`/coins/${coin.id}`}> {coin.high_24h}</Link>
-              </td>
-              <td className="border-r-2 text-center px-1 py-2 hid ">
-                <Link to={`/coins/${coin.id}`}> {coin.total_volume}</Link>
-              </td>
+                    <p className="uppercase text-gray-500 text-md overflow-hidden md:mx-2 ">
+                      {coin.symbol}
+                    </p>
+                  </Link>
+                </td>
+                <td className="border-r-2 text-center px-1 py-2 ">
+                  <Link to={`/coins/${coin.id}`}> {coin.current_price}</Link>
+                </td>
+                <td className="border-r-2 text-center px-1 py-2 hid ">
+                  <Link to={`/coins/${coin.id}`}> {coin.low_24h}</Link>
+                </td>
+                <td className="border-r-2 text-center px-1 py-2 hid ">
+                  <Link to={`/coins/${coin.id}`}> {coin.high_24h}</Link>
+                </td>
+                <td className="border-r-2 text-center px-1 py-2 hid ">
+                  <Link to={`/coins/${coin.id}`}> {coin.total_volume}</Link>
+                </td>
 
-              <td className="border-r-2 text-center  py-2  ">
-                <Link to={`/coins/${coin.id}`}> {coin.market_cap}</Link>
-              </td>
-            </tr>
-          ))}
+                <td className="border-r-2 text-center  py-2  ">
+                  <Link to={`/coins/${coin.id}`}> {coin.market_cap}</Link>
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </>
   );
