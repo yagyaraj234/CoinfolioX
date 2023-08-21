e90e1df322ec4fc9facee222039a24f693c0fc30
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:11:01 +0530

    removed articles section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import NewsCard from '../components/NewsCard'
-import CoinList from '../components/CoinList'
-import { NavLink } from 'react-router-dom'
-
+import React from "react";
+import NewsCard from "../components/NewsCard";
+import CoinList from "../components/CoinList";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <NewsCard />
-      <CoinList amount={50}/>
-      <button className=' flex justify-center mx-auto  px-2 py-1 rounded-md bg-blue-600 text-white hover:bg-blue-700 w-2/4 my-5 md:py-2 md:w-1/4'><NavLink to='/cryptocurrencies'>More Coins</NavLink></button>
+      {/* <NewsCard /> */}
+      <CoinList />
+      <button className=" flex justify-center mx-auto  px-2 py-1 rounded-md bg-blue-600 text-white hover:bg-blue-700 w-2/4 my-5 md:py-2 md:w-1/4">
+        <NavLink to="/cryptocurrencies">More Coins</NavLink>
+      </button>
     </>
-  )
-}
+  );
+};
 
 export default Home;
