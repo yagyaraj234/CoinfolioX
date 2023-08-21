6cccd38f9dd60befc93474fa17e5aef58c64f424
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:10:34 +0530

    login button and currency change dropdown added

--- frontend/src/components/Layout/MobHeader.jsx
+++ frontend/src/components/Layout/MobHeader.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
-
+import { useToken } from "../Context/Token";
+import Dropdown from "../Dropdown";
 const MobHeader = () => {
   const [menu, setMenu] = useState("hidden");
+  const { token } = useToken();
 
   const showMenu = () => {
     if (menu === "hidden") {
@@ -22,7 +24,9 @@ const MobHeader = () => {
 
         {menu === "hidden" ? (
           <RxHamburgerMenu className="text-4xl font-light" onClick={showMenu} />
-        ):<RxCross2 className="text-4xl font-light "onClick={showMenu} />}
+        ) : (
+          <RxCross2 className="text-4xl font-light " onClick={showMenu} />
+        )}
       </div>
       <div className="px-2">
         <ul className={`flex flex-col py-2 ${menu}`}>
@@ -35,12 +39,17 @@ const MobHeader = () => {
           <NavLink className="border-b-2 py-2 border-gray-100" to="/trending">
             Trending
           </NavLink>
-          <NavLink
-            className="border-b-2 py-2 border-gray-100"
-            to="/cryptocurrencies"
-          >
-            Cryptocurrencies
-          </NavLink>
+          {/* <Link className="border-b-2 font-semibold py-2" to="/login">Login</Link> */}
+          {token !== null ? (
+            <Link to="/profile" className="border-b-2 font-semibold py-2">
+              Profile
+            </Link>
+          ) : (
+            <Link className="border-b-2 font-semibold py-2" to="/login">
+              Login
+            </Link>
+          )}
+          <Dropdown />
         </ul>
       </div>
     </>
