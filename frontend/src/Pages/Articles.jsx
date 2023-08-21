c97e813b9273f1c89fb5a8aa4a9f4bcb1ed2828f
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:06:10 +0530

    articles

--- frontend/src/Pages/Articles.jsx
+++ frontend/src/Pages/Articles.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import articleData from "../article.json";
 
 const Articles = () => {
   return (
     <>
-      Articles
+      <div className="  text-xl font-semibold border-b-2">Article</div>
+      {articleData.map((article) => (
+        <div className="my-3 py-4 px-2 rounded-lg text-justify " key={article.id}>
+          <p className="font-semibold text-lg">{article.title}</p>
+          <p>{article.desc}</p>
+          <p className="font-semibold text-primary text-end ">~ {article.user}</p>
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Articles
+export default Articles;
