bc5c9e675ec47fdcf1524908121f6bb11a2c2640
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-22 02:05:25 +0530

    new colors added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#F9F7F7",
+        primary: "#112D4E",
+        darkgray: "#DBE2EF",
+        lightnavy: "#3F72AF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
