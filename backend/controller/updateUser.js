182ccfb7733af23e3ddd32e3b9358d365730d63e
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-08-29 23:47:05 +0530

    update user backend

--- backend/controller/updateUser.js
+++ backend/controller/updateUser.js
@@ -1,25 +1,38 @@
-const model = require("../models/UserAccount");
-const user = model.UserModel;
+// const model = require("../models/UserAccount");
+// const user = model.UserModel;
+import { userSchema } from "../models/userData.js";
 
 const updateUser = async (req, res) => {
-  const id = req.params.id;
+  const token = req.token;
+  const updateData = req.body;
+  console.log(updateData);
   try {
-    const updateUser = await user.findOneAndUpdate({ _id: id }, req.body, );
-    res.json({ message: "success", updateUser });
+    const updatedUser = await userSchema.findOneAndUpdate(
+      { jwt_token: token },
+      { $set: updateData }, // Use $set to update specific fields
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "success", user: updatedUser });
   } catch (error) {
-    res.json({ message: "error" });
+    res.json({ message: "error", error: error });
   }
 };
 
-const replaceUser = async (req, res) => {
-  const id = req.params.id;
-  try {
-    const replaceUser = await user.findOneAndReplace({ _id: id }, req.body,{new:true});
-    res.json({ message: "success" });
-  } catch (error) {
-    res.json({ message: "error" });
-  }
-};
+export default updateUser;
+// const replaceUser = async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const replaceUser = await user.findOneAndReplace({ _id: id }, req.body, {
+//       new: true,
+//     });
+//     res.json({ message: "success" });
+//   } catch (error) {
+//     res.json({ message: "error" });
+//   }
+// };
 
-module.exports = updateUser;
-module.exports = replaceUser;
+// module.exports = replaceUser;
